e4c7e62adea009a2732acd82efd65d5bec364792
Usama Sadiq <=>
2020-12-19 23:59:35 +0500

    Update app.js

--- chap_9/app.js
+++ chap_9/app.js
@@ -1,5 +1,5 @@
-var firstName = prompt("Enter first name", "Type Here...");
-var lastName = prompt("Enter last name", "Type Here...");
+var firstName = prompt("Enter your first name", "Type Here...");
+var lastName = prompt("Enter your last name", "Type Here...");
 var age = +prompt("Enter your age", "Type Here...");
 
 alert('Hello ' + firstName + ' ' + lastName + ', You are ' + age + ' years old');
